94a45e3972176d662dedcb6238c13f10dfacba43
GwonTaekJoon <kyuzu123123@gmail.com>
2023-04-25 19:20:46 +0900

    2023-04-25

--- baekjoon using cpp/main.cpp
+++ baekjoon using cpp/main.cpp
@@ -2103,3 +2103,56 @@ int main() {
 */
 
 
+
+#include <iostream>
+#include <algorithm>
+//boj 10844
+using namespace std;
+
+
+long long dp[101][101] = { 0, };
+
+int main() {
+
+    int n;
+    long long sum = 0;
+    cin >> n;
+    dp[1][0] = 0;
+    for (size_t i = 1; i <= 9; ++i) {
+        dp[1][i] = 1;
+        
+    }
+
+    for (size_t i = 2; i <= n; ++i) {
+
+        for (size_t j = 0; j <= 9; ++j) {
+            
+            if (j == 9 || j == 0) {
+                if (j == 9)
+                    dp[i][j] = dp[i - 1][j - 1] % 1000000000;
+                else
+                    dp[i][j] = dp[i - 1][j + 1] % 1000000000;
+            }
+            else {
+                dp[i][j] = dp[i - 1][j - 1] % 1000000000 + dp[i - 1][j + 1] % 1000000000;
+            }
+           
+
+        }
+        
+        
+
+    }
+
+
+    for (int i = 0; i < 10; ++i) {
+    
+        sum += dp[n][i] % 1000000000;
+
+    }
+
+
+    cout << sum % 1000000000;
+
+    return 0;
+}
