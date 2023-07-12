554918858af97a7f771ed19dff03c5be9a81a514
GwonTaekJoon <kyuzu123123@gmail.com>
2023-07-12 15:46:19 +0900

    2023-07-12

--- baekjoon using cpp/main.cpp
+++ baekjoon using cpp/main.cpp
@@ -3058,3 +3058,50 @@ int main() {
 }*/
 
 
+/*
+#include <iostream>
+#include <algorithm>
+//boj 2225
+using namespace std;
+int dp[201][201] = { 0, };
+int main() {
+
+    int n, k;
+    cin >> n >> k;
+
+
+    for (int i = 1; i <= n; ++i) {
+       
+        for (int j = 1; j <= k; ++j) {
+            
+
+            if (i == 1) {
+
+                dp[i][j] = j;
+                continue;
+            
+            }
+            if (j == 1) {
+
+                dp[i][j] = 1;
+                continue;
+            }
+
+            dp[i][j] = (dp[i - 1][j] + dp[i][j - 1]) % 1000000000;
+
+        
+        
+        }
+
+    
+    }
+
+    cout << dp[n][k];
+
+
+
+    return 0;
+
+}
+
+*/
